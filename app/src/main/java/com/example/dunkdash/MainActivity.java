9e11034a28672c3dd790839255eb2e6a2119bb4e
Lali <168171450+LaliRabaev@users.noreply.github.com>
2025-05-27 20:03:58 +0300

    a

--- app/src/main/java/com/example/dunkdash/MainActivity.java
+++ app/src/main/java/com/example/dunkdash/MainActivity.java
@@ -1,17 +1,24 @@
+package com.example.dunkdash;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
 import com.google.android.gms.ads.MobileAds;
 import com.google.android.gms.ads.initialization.InitializationStatus;
 import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
 
-@Override
-protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
+public class MainActivity extends AppCompatActivity {
     
-    // Initialize the Mobile Ads SDK
-    MobileAds.initialize(this, new OnInitializationCompleteListener() {
-        @Override
-        public void onInitializationComplete(InitializationStatus initializationStatus) {
-            // SDK initialization completed
-        }
-    });
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+        // Initialize Mobile Ads
+        MobileAds.initialize(this, new OnInitializationCompleteListener() {
+            @Override
+            public void onInitializationComplete(InitializationStatus initializationStatus) {
+                // Mobile Ads initialization is complete
+            }
+        });
+    }
 }
